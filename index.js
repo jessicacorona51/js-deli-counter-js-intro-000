42b38083a08b32865791a777da602eb25e51c4d4
Jessica Corona <jessicacorona51@yahoo.com>
2020-06-13 03:25:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ function nowServing(katzDeliLine) {
 function currentLine (line) {
   if (line.length === 0) {
     return ("The line is currently empty.");
+  } else {
+    var array = [];
+    for (var i = 0; i < line.length; i++) {
+      array.push(`${i+1}. ${line[i]}`);
+    }
+    return ("The line is currently:" + array) ;
   }
-
 }
