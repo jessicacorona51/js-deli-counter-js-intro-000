1726f31e881f6fc1ddba9047dd23caa317b508fd
Jessica Corona <jessicacorona51@yahoo.com>
2020-06-13 03:03:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeliLine = []
 function takeANumber(katzDeliLine, name) {
-  katsDeliLine.push (name)
+  katzDeliLine.push (name)
   return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
 }
 
