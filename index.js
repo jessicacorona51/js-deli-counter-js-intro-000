8f46a1c48ad074a36411389cdcd457d61a4920f8
Jessica Corona <jessicacorona51@yahoo.com>
2020-06-13 03:01:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 var katzDeliLine = []
-function takeANumber(katsDeliLine, name) {
+function takeANumber(katzDeliLine, name) {
   katsDeliLine.push (name)
-  return (`Welcome, ${name}. You are number ${katsDeliLine.length} in line.`);
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
+}
+
+function nowServing(katzDeliLine) {
+  let i = 0
+  while (i < katzDeliLine.length) {
+    i++;
+  }
+  if (katzDeliLine.length === 0) {
+    return ("There is nobody waiting to be served!")
+  }
 }
